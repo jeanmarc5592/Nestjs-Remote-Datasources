3aeedd9ba678f50b740d699c86b351e28f6840d5
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2024-08-18 13:14:48 +0200

    Setup compodoc

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DataSourcesModule } from './data-sources/data-sources.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [DataSourcesModule],
+  imports: [
+    DataSourcesModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'documentation'),
+      serveRoot: '/docs',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
